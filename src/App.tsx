82afa21ea8a3379043bce7099eae11a93b1f50e7
Tyler Kilburn <tylerkilburn@carfax.com>
2024-03-23 15:22:06 -0400

    Updates to support multiple lists

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,20 @@ import groceries from "./data/groceries.json"
 import { JSXElement } from "solid-js"
 import { IShoppingItem } from "@types"
 
+const BLANK_TEXT = "__________"
 const ITEM_LIMIT = 44
 
+//@ts-ignore
+
 const App = () => {
-  return <div class={styles.root}>{renderShoppingCards(6)}</div>
+  const data = matchDataToListName(import.meta.env.VITE_LIST_NAME)
+  return <div class={styles.root}>{renderShoppingCards(6, data)}</div>
 }
 
-const renderShoppingCards = (num: number) => {
+const renderShoppingCards = (num: number, data: IShoppingItem[]) => {
   const jsx: JSXElement = []
   for (let i = 0; i < num; i++) {
-    jsx.push(<ShoppingCard shoppingItems={truncateShoppingItems(groceries)} />)
+    jsx.push(<ShoppingCard shoppingItems={truncateShoppingItems(data)} />)
   }
   return jsx
 }
@@ -22,9 +26,18 @@ const renderShoppingCards = (num: number) => {
 const truncateShoppingItems = (shoppingItems: IShoppingItem[]) => {
   const truncatedShoppingItems = shoppingItems.slice(0, ITEM_LIMIT)
   for (let i = truncatedShoppingItems.length; i < ITEM_LIMIT; i++) {
-    truncatedShoppingItems.push({ category: "", name: "" })
+    truncatedShoppingItems.push({ category: "", name: BLANK_TEXT })
   }
   return truncatedShoppingItems
+}
+
+const matchDataToListName = (listName: string) => {
+  switch (listName) {
+    case "grocery":
+    case "groceries":
+      return groceries
+  }
+  return []
 }
 
 export default App
